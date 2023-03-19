f7c732782fb57f4147a86566c4080c8eeb5b875f
GhazanfarShahbaz <ghazanfar.shahbaz95@myhunter.cuny.edu>
2023-03-19 18:09:30 -0400

    rotate inputs

--- rotate.py
+++ rotate.py
@@ -1,15 +1,18 @@
 import PyPDF4
+from os import getcwd, path
 
-pdf_in = open('/Users/ghaz/documents/schoolwork/other/asian210/readings/Unthinking_Eurocentrism.pdf', 'rb')
+pdf_in = open(path.join(input("Enter a file directory: "), input("Enter the file name: ")), 'rb')
 pdf_reader = PyPDF4.PdfFileReader(pdf_in)
 pdf_writer = PyPDF4.PdfFileWriter()
 
+
+rotate_num: int = int(input("How much to rotate clockwise? "))
 for pagenum in range(pdf_reader.numPages):
     page = pdf_reader.getPage(pagenum)
-    page.rotateClockwise(90)
+    page.rotateClockwise(rotate_num)
     pdf_writer.addPage(page)
 
-pdf_out = open('/Users/ghaz/documents/schoolwork/other/asian210/readings/UnthinkingEurocentrism.pdf', 'wb')
+pdf_out = open(path.join(getcwd(), input("Enter an output file name: ")), 'wb')
 pdf_writer.write(pdf_out)
 pdf_out.close()
 pdf_in.close()
